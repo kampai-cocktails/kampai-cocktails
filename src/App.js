4be0b15bb1a9c20cd4c5373a96d784e76ca5925d
Kapakahi <rubbishman@gmail.com>
2020-05-20 11:34:16 +0900

    added frames for weather and clock and a little css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./styles/App.css";
 import Navbar from "./components/Navbar.jsx"
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      
+
     </div>
   );
 }
---
+++ src/components/Clock.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Clock() {
+  return (
+    <div className='Clock'>
+      <h2>tick tock tick tock</h2>
+      <iframe src="http://free.timeanddate.com/clock/i7aivsiv/n248/szw110/szh110/hoc9b8578/hbw10/hfc754c29/cf100/hnc432f30/fav0/fiv0/mqcfff/mqs4/mql25/mqw12/mqd78/mhcfff/mhs2/mhl5/mhw2/mhd78/mmv0/hhcfff/hhs2/hhl50/hhw8/hmcfff/hms2/hml70/hmw8/hmr4/hscfff/hss3/hsl70/hsw3" frameborder="0" width="110" height="110"></iframe>
+    </div>
+  )
+}
