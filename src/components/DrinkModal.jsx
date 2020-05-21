cbd29806e8fbb7d3e9f4195d62f51ae7742b82aa
Halee Pagel <halee.pagel@gmail.com>
2020-05-21 13:48:49 +0900

    even more css

--- src/components/DrinkModal.jsx
+++ src/components/DrinkModal.jsx
@@ -15,11 +15,14 @@ function checkIngredients(fullDrinkInfo) {
 
   // return newArray;
   return (
-    <ul>
-      {newArray.map((ingredient, index) => (
-        <li key={index}>{ingredient}</li>
-      ))}
-    </ul>
+    <div className="ingredientList">
+      Ingredients
+      <ul className="ingredientList">
+        {newArray.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
@@ -28,18 +31,29 @@ function DrinkModal(props) {
     <div className="modal">
       <div className="modal-content">
         <h3>IM A SEXY MODAL</h3>
-        <img src={props.fullDrinkInfo.strDrinkThumb}></img>
-        <div>{props.fullDrinkInfo.strDrink}</div>
+        <img
+          className="modalImage"
+          src={props.fullDrinkInfo.strDrinkThumb}
+        ></img>
+        <div className="drinkName">{props.fullDrinkInfo.strDrink}</div>
         <hr></hr>
-        <p>Is there alcohol? {props.fullDrinkInfo.strAlcoholic}</p>
-        <p>Type of glass? {props.fullDrinkInfo.strGlass}</p>
+        <p className="alcoholContent">
+          Is there alcohol? {props.fullDrinkInfo.strAlcoholic}
+        </p>
+        <p className="glassType">
+          Type of glass? {props.fullDrinkInfo.strGlass}
+        </p>
 
-        <p>Category: {props.fullDrinkInfo.strCategory} </p>
+        <p className="drinkCategory">
+          Category: {props.fullDrinkInfo.strCategory}{" "}
+        </p>
 
         <hr></hr>
         {checkIngredients(props.fullDrinkInfo)}
         <hr></hr>
-        <p>Instructions: {props.fullDrinkInfo.strInstructions}</p>
+        <p className="instructions">
+          Instructions: {props.fullDrinkInfo.strInstructions}
+        </p>
         <button
           type="button"
           onClick={() => {
