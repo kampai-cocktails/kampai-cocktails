586525f142080d1c819fba3f45261f1473068945
softwaredeveloptam <39637317+softwaredeveloptam@users.noreply.github.com>
2020-05-21 23:33:06 +0900

    Fixed the clock

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/Clock.css";
+import ReactAnime from "react-animejs";
+const { Anime, stagger } = ReactAnime;
 
 export default function () {
   return (
@@ -7,7 +9,19 @@ export default function () {
       <div className="clock-face">
         <div className="hour-hand"></div>
         <div className="min-hand"></div>
-        <div className="sec-hand"></div>
+        <Anime
+          initial={[
+            {
+              targets: "#secHand",
+              rotate: "1turn",
+              duration: 60000,
+              easing: "linear",
+              loop: true,
+            },
+          ]}
+        >
+          <div className="sec-hand" id="secHand"></div>
+        </Anime>
       </div>
     </div>
   );
