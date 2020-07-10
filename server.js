af9361f2581fd7a3d46dc0efc3ac8f3fd7de085f
softwaredeveloptam <39637317+softwaredeveloptam@users.noreply.github.com>
2020-07-10 15:25:25 +0900

    fixed the server

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, "..", "kampai-cocktails\\public");
+// const publicPath = path.join(__dirname, "..", ".\\Kampai-Cocktails\\public");
 
 // Serve the static files from the React app
-app.use("", express.static(publicPath));
+app.use(express.static(path.join(__dirname, "build")));
 
 // // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
+  res.sendFile(path.join(__dirname, "build/index.html"));
 });
 
 app.listen(port, () => {
