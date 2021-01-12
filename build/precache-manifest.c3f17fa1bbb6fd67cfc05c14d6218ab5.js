1f02bbb3d1eb9d78732d428b2902f0e3c0764466
Halee Pagel <halee.pagel@gmail.com>
2021-01-12 10:23:32 +0900

    fiddled with minor CSS and created a build package for web deployment

---
+++ build/precache-manifest.c3f17fa1bbb6fd67cfc05c14d6218ab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42015d0a910751b98f7f2da9b982f619",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1becd3926f010e5d2e52",
+    "url": "/static/css/main.aeddd8d9.chunk.css"
+  },
+  {
+    "revision": "418eb19712432fbd9d8f",
+    "url": "/static/js/2.63963770.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.63963770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1becd3926f010e5d2e52",
+    "url": "/static/js/main.ee5eb014.chunk.js"
+  },
+  {
+    "revision": "3387c3874dac5883c053",
+    "url": "/static/js/runtime-main.ee4f3a6f.js"
+  }
+]);
